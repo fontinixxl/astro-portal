f2d9b497873a8dfcb090bf155f44ac73091be072
Gerard Cuello <gerard.urv@gmail.com>
2019-12-25 15:05:00 +0000

    Implement player animation for walking and jumping

--- Assets/PlayerController2D.cs
+++ Assets/PlayerController2D.cs
@@ -7,38 +7,56 @@ public class PlayerController2D : MonoBehaviour
     // LayerMask we are using to check if we are grounded.
     [SerializeField] private LayerMask platformsLayerMask;
 
-    public float moveSpeed = 40f;
-    public float jumpVelocity = 100f;
-    private float horizontalSpeed;
+    public float moveSpeed = 5;
+    public float jumpVelocity = 11;
+    private float horizontalDirection;
     private Rigidbody2D rigidbody2d;
     private BoxCollider2D boxCollider2d;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
 
     private void Awake()
     {
         rigidbody2d = transform.GetComponent<Rigidbody2D>();
         boxCollider2d = transform.GetComponent<BoxCollider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer> ();
+        animator = GetComponent<Animator> ();
+        // DEBUG
+        rigidbody2d.gravityScale = 3.5f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        bool grounded = IsGrounded();
+        if (grounded && Input.GetKeyDown(KeyCode.Space))
         {
             rigidbody2d.velocity = Vector2.up * jumpVelocity;
         }
 
-        horizontalSpeed = Input.GetAxis("Horizontal") * moveSpeed;
+        horizontalDirection = Input.GetAxis("Horizontal");
+
+        bool flipSprite = (spriteRenderer.flipX ? (horizontalDirection > 0.01f) : (horizontalDirection < 0f));
+        if (flipSprite)
+        {
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+        }
+
+        animator.SetFloat ("velocityX", Mathf.Abs (rigidbody2d.velocity.x) / moveSpeed);
+        animator.SetBool("jumping", !grounded);
     }
 
     void FixedUpdate()
     {
-        rigidbody2d.velocity = new Vector2(horizontalSpeed, rigidbody2d.velocity.y);
+        rigidbody2d.velocity = new Vector2(horizontalDirection * moveSpeed, rigidbody2d.velocity.y);
 
     }
 
     private bool IsGrounded()
     {
-        RaycastHit2D raycastHit2d = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down, 1f, platformsLayerMask);
+        RaycastHit2D raycastHit2d = Physics2D.BoxCast(boxCollider2d.bounds.center,
+            boxCollider2d.bounds.size, 0f, Vector2.down, 1f, platformsLayerMask);
+
         return raycastHit2d.collider != null;
     }
 
---
+++ Assets/SimpleJump/Scripts/Player.cs
@@ -0,0 +1,116 @@
+﻿/* 
+    ------------------- Code Monkey -------------------
+
+    Thank you for downloading this package
+    I hope you find it useful in your projects
+    If you have any questions let me know
+    Cheers!
+
+               unitycodemonkey.com
+    --------------------------------------------------
+ */
+
+using System;
+using UnityEngine;
+
+/*
+ * Simple Jump
+ * */
+public class Player : MonoBehaviour {
+
+    [SerializeField] private LayerMask platformsLayerMask;
+    // private Player_Base playerBase;
+    private Rigidbody2D rigidbody2d;
+    private BoxCollider2D boxCollider2d;
+
+    private void Awake() {
+        // playerBase = gameObject.GetComponent<Player_Base>();
+        rigidbody2d = transform.GetComponent<Rigidbody2D>();
+        boxCollider2d = transform.GetComponent<BoxCollider2D>();
+    }
+
+    private void Update() {
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space)) {
+            float jumpVelocity = 100f;
+            rigidbody2d.velocity = Vector2.up * jumpVelocity;
+        }
+
+        HandleMovement_FullMidAirControl();
+        //HandleMovement_SomeMidAirControl();
+        //HandleMovement_NoMidAirControl();
+
+        // Set Animations
+        if (IsGrounded()) {
+            if (rigidbody2d.velocity.x == 0) {
+                // playerBase.PlayIdleAnim();
+            } else {
+                // playerBase.PlayMoveAnim(new Vector2(rigidbody2d.velocity.x, 0f));
+            }
+        } else {
+            // playerBase.PlayJumpAnim(rigidbody2d.velocity);
+        }
+    }
+
+    private bool IsGrounded() {
+        RaycastHit2D raycastHit2d = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down, 1f, platformsLayerMask);
+        return raycastHit2d.collider != null;
+    }
+    
+    private void HandleMovement_FullMidAirControl() {
+        float moveSpeed = 40f;
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            rigidbody2d.velocity = new Vector2(-moveSpeed, rigidbody2d.velocity.y);
+        } else {
+            if (Input.GetKey(KeyCode.RightArrow)) {
+                rigidbody2d.velocity = new Vector2(+moveSpeed, rigidbody2d.velocity.y);
+            } else {
+                // No keys pressed
+                rigidbody2d.velocity = new Vector2(0, rigidbody2d.velocity.y);
+            }
+        }
+    }
+
+    private void HandleMovement_SomeMidAirControl() {
+        float moveSpeed = 40f;
+        float midAirControl = 3f;
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            if (IsGrounded()) {
+                rigidbody2d.velocity = new Vector2(-moveSpeed, rigidbody2d.velocity.y);
+            } else {
+                rigidbody2d.velocity += new Vector2(-moveSpeed * midAirControl * Time.deltaTime, 0);
+                rigidbody2d.velocity = new Vector2(Mathf.Clamp(rigidbody2d.velocity.x, -moveSpeed, +moveSpeed), rigidbody2d.velocity.y);
+            }
+        } else {
+            if (Input.GetKey(KeyCode.RightArrow)) {
+                if (IsGrounded()) {
+                    rigidbody2d.velocity = new Vector2(+moveSpeed, rigidbody2d.velocity.y);
+                } else {
+                    rigidbody2d.velocity += new Vector2(+moveSpeed * midAirControl * Time.deltaTime, 0);
+                    rigidbody2d.velocity = new Vector2(Mathf.Clamp(rigidbody2d.velocity.x, -moveSpeed, +moveSpeed), rigidbody2d.velocity.y);
+                }
+            } else {
+                // No keys pressed
+                if (IsGrounded()) {
+                    rigidbody2d.velocity = new Vector2(0, rigidbody2d.velocity.y);
+                }
+            }
+        }
+    }
+
+    private void HandleMovement_NoMidAirControl() {
+        if (IsGrounded()) {
+            float moveSpeed = 40f;
+            if (Input.GetKey(KeyCode.LeftArrow)) {
+                rigidbody2d.velocity = new Vector2(-moveSpeed, rigidbody2d.velocity.y);
+            } else {
+                if (Input.GetKey(KeyCode.RightArrow)) {
+                    rigidbody2d.velocity = new Vector2(+moveSpeed, rigidbody2d.velocity.y);
+                } else {
+                    // No keys pressed
+                    rigidbody2d.velocity = new Vector2(0, rigidbody2d.velocity.y);
+                }
+            }
+        }
+    }
+
+}
